5b2851b783b4fb1cc80401eea4b13a978b499f41
Eduardo Salvador <eduardo.salvador@col.vueling.com>
2021-09-03 12:23:49 +0200

    trest Humans_can_reproduce_when_there_is_a_name_a_mother_and_a_father resolved

--- src/Singleton-kata/Singleton-kata/Program.cs
+++ src/Singleton-kata/Singleton-kata/Program.cs
@@ -11,7 +11,7 @@ namespace Singleton_kata
 		{
 			private static Adam _adam;
 
-			private Adam() { }
+			private Adam() : base("Adam", null, null) { }
 
 			public static Adam GetInstance()
 			{
@@ -26,7 +26,7 @@ namespace Singleton_kata
 			private Adam _adam;
 			private static Eve _eve;
 
-			private Eve(Adam adam)
+			private Eve(Adam adam) : base("Eve", null, null)
 			{
 				if (adam == null)
 					throw new ArgumentNullException("Eva needs a rib of Adam to be born");
@@ -41,11 +41,33 @@ namespace Singleton_kata
 			}
 		}
 
-		public class Male : Human { }
+		public class Male : Human
+		{
+			public Male(string name, Female mother, Male father) : base(name, mother, father)
+			{
+			}
+		}
 
-		public class Female : Human { }
+		public class Female : Human
+		{
+			public Female(string name, Female mother, Male father) : base(name, mother, father)
+			{
+			}
+		}
 
-		public abstract class Human { }
+		public abstract class Human
+		{
+			public readonly string Name;
+			public readonly Female Mother;
+			public readonly Male Father;
+
+			public Human(string name, Female mother, Male father)
+			{
+				Name = name;
+				Mother = mother;
+				Father = father;
+			}
+		}
 
 		public static class SampleTests
 		{
@@ -127,50 +149,51 @@ namespace Singleton_kata
 			{
 				Assert.IsTrue(Eve.GetInstance(Adam.GetInstance()) is Female);
 			}
+
+			public static void Reproduction_always_result_in_a_male_or_female()
+			{
+				Assert.IsTrue(typeof(Human).IsAbstract);
+			}
+
+			public static void Humans_can_reproduce_when_there_is_a_name_a_mother_and_a_father()
+			{
+				var adam = Adam.GetInstance();
+				var eve = Eve.GetInstance(adam);
+				var seth = new Male("Seth", eve, adam);
+				var azura = new Female("Azura", eve, adam);
+				var enos = new Male("Enos", azura, seth);
+
+				Assert.AreEqual("Eve", eve.Name);
+				Assert.AreEqual("Adam", adam.Name);
+				Assert.AreEqual("Seth", seth.Name);
+				Assert.AreEqual("Azura", azura.Name);
+				Assert.AreEqual("Enos", enos.Name);
+				Assert.AreEqual(seth, enos.Father);
+				Assert.AreEqual(azura, enos.Mother);
+			}
+
 			/*
-												public static void Reproduction_always_result_in_a_male_or_female()
-												{
-													Assert.IsTrue(typeof(Human).IsAbstract);
-												}
+															public static void Father_and_mother_are_essential_for_reproduction()
+															{
+																// There is just 1 way to reproduce
+																Assert.AreEqual(1, typeof(Male).GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+																  .Where(x => x.IsPublic || x.IsAssembly).Count());
+																Assert.AreEqual(1, typeof(Female).GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).
+																  Where(x => x.IsPublic || x.IsAssembly).Count());
 
-												public static void Humans_can_reproduce_when_there_is_a_name_a_mother_and_a_father()
-												{
-													var adam = Adam.GetInstance();
-													var eve = Eve.GetInstance(adam);
-													var seth = new Male("Seth", eve, adam);
-													var azura = new Female("Azura", eve, adam);
-													var enos = new Male("Enos", azura, seth);
-
-													Assert.AreEqual("Eve", eve.Name);
-													Assert.AreEqual("Adam", adam.Name);
-													Assert.AreEqual("Seth", seth.Name);
-													Assert.AreEqual("Azura", azura.Name);
-													Assert.AreEqual("Enos", ((Human)enos).Name);
-													Assert.AreEqual(seth, ((Human)enos).Father);
-													Assert.AreEqual(azura, ((Human)enos).Mother);
-												}
-
-												public static void Father_and_mother_are_essential_for_reproduction()
-												{
-													// There is just 1 way to reproduce
-													Assert.AreEqual(1, typeof(Male).GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-													  .Where(x => x.IsPublic || x.IsAssembly).Count());
-													Assert.AreEqual(1, typeof(Female).GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).
-													  Where(x => x.IsPublic || x.IsAssembly).Count());
-
-													var adam = Adam.GetInstance();
-													var eve = Eve.GetInstance(adam);
-													Assert.Throws<ArgumentNullException>(() => new Male("Seth", null, null));
-													Assert.Throws<ArgumentNullException>(()=> new Male("Abel", eve, null));
-													Assert.Throws<ArgumentNullException>(() => new Male("Seth", null, adam));
-													Assert.Throws<ArgumentNullException>(() => new Female("Azura", null, null));
-													Assert.Throws<ArgumentNullException>(() => new Female("Awan", eve, null));
-													Assert.Throws<ArgumentNullException>(() => new Female("Dina", null, adam));
-													Assert.Throws<ArgumentNullException>(() => new Female("Eve", null, null));
-													Assert.Throws<ArgumentNullException>(() => new Male("Adam", null, null));
-												}
-												}
-												*/
+																var adam = Adam.GetInstance();
+																var eve = Eve.GetInstance(adam);
+																Assert.Throws<ArgumentNullException>(() => new Male("Seth", null, null));
+																Assert.Throws<ArgumentNullException>(()=> new Male("Abel", eve, null));
+																Assert.Throws<ArgumentNullException>(() => new Male("Seth", null, adam));
+																Assert.Throws<ArgumentNullException>(() => new Female("Azura", null, null));
+																Assert.Throws<ArgumentNullException>(() => new Female("Awan", eve, null));
+																Assert.Throws<ArgumentNullException>(() => new Female("Dina", null, adam));
+																Assert.Throws<ArgumentNullException>(() => new Female("Eve", null, null));
+																Assert.Throws<ArgumentNullException>(() => new Male("Adam", null, null));
+															}
+															}
+															*/
 
 			private static void Main(string[] args)
 			{
@@ -182,8 +205,10 @@ namespace Singleton_kata
 
 				//SampleTests.Eve_is_unique_and_created_from_a_rib_of_adam();
 				//SampleTests.Eve_can_only_be_create_of_a_rib_of_adam();
-				SampleTests.Eve_is_a_human();
-				SampleTests.Eve_is_a_female();
+				//SampleTests.Eve_is_a_human();
+				//SampleTests.Eve_is_a_female();
+				//SampleTests.Reproduction_always_result_in_a_male_or_female();
+				SampleTests.Humans_can_reproduce_when_there_is_a_name_a_mother_and_a_father();
 				Console.WriteLine("Hello World!");
 			}
 		}
